2665a246a75b3703818c3124064affa48dcf1d68
kolomigor <kolomigor46@yandex.ru>
2025-12-30 01:10:32 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
+#include "mesh_data.h"
 #include <iostream>
 
+MeshData CreateTriangle() {
+    MeshData mesh;
+    mesh.vertices = {
+        {{0.0f, 0.0f, 0.0f}}, {{1.0f, 0.0f, 0.0f}}, {{0.0f, 1.0f, 0.0f}}};
+    mesh.indices = {0, 1, 2};
+    return mesh;
+}
+
 int main() {
-    std::cout << "There will be a 3D renderer" << std::endl;
+    std::cout << "3D Renderer begins here" << std::endl;
+    MeshData triangle = CreateTriangle();
+    std::cout << "Triangle vertices: " << triangle.vertices.size() << std::endl;
     return 0;
 }

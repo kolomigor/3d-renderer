fc27c22b7cab69ea0e79c29fa43ef469c37ce750
kolomigor <kolomigor46@yandex.ru>
2025-12-30 03:18:57 +0300

    Add basic OpenGL logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,60 @@
-#include "mesh_data.h"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-MeshData CreateTriangle() {
-    MeshData mesh;
-    mesh.vertices = {
-        {{0.0f, 0.0f, 0.0f}}, {{1.0f, 0.0f, 0.0f}}, {{0.0f, 1.0f, 0.0f}}};
-    mesh.indices = {0, 1, 2};
-    return mesh;
-}
-
 int main() {
-    std::cout << "3D Renderer begins here" << std::endl;
-    MeshData triangle = CreateTriangle();
-    std::cout << "Triangle vertices: " << triangle.vertices.size() << std::endl;
+    // Init GLFW
+    if (!glfwInit()) {
+        std::cerr << "GLFW init failed" << std::endl;
+        return 1;
+    }
+
+    // OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // Core profile
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // MacOS restriction for core profile
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // Create window
+    GLFWwindow *window =
+        glfwCreateWindow(800, 600, "3D Renderer", nullptr, nullptr);
+
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        return 1;
+    }
+
+    // Create context
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    // Load GLAD
+    auto loader = reinterpret_cast<GLADloadproc>(glfwGetProcAddress);
+    if (!gladLoadGLLoader(loader)) {
+        std::cerr << "Failed to load GLAD" << std::endl;
+        return 1;
+    }
+
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
+    glViewport(0, 0, 800, 600);
+
+    // Main cycle
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+
+    // Close GLFW
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }

f26bf9270fcbb4dd92a7077fd14cc039834e0487
kolomigor <kolomigor46@yandex.ru>
2026-01-13 06:54:03 +0300

    First commit

---
+++ src/application.h
@@ -0,0 +1,20 @@
+#include "camera.h"
+#include "lights.h"
+#include "picture.h"
+#include "renderer.h"
+#include "screen.h"
+#include "world.h"
+
+class Application {
+public:
+	Application();
+	void Run();
+
+private:
+	World world_;
+	Camera camera_;
+	Lights lights_;
+	Renderer renderer_;
+	Picture picture_;
+	Screen screen_;
+};
